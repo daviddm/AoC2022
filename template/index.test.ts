1ce970c78a1a92ec94faeae9c2ce1efd225b6ac7
David de Meijere <david.de.meijere@gmail.com>
2022-12-01 06:35:40 +0100

    extended templatte

--- template/index.test.ts
+++ template/index.test.ts
@@ -1,8 +1,22 @@
+import { readFile } from "@util/readFile";
+import { stringToNumber } from "@util/stringToNumber";
 import { run } from "./";
 
 describe("1", () => {
-  it("should be true", async () => {
-    await run();
-    expect(true).toBeTruthy();
+  let list: number[];
+  let testList: number[];
+
+  beforeEach(async () => {
+    list = stringToNumber(await readFile("./1/input.txt"));
+    testList = stringToNumber(await readFile("./1/test.txt"));
   });
+
+  describe("test", () => {
+    it("run", async () => {
+      await run();
+      expect(true).toBeTruthy();
+    });
+  });
+  describe.skip("first", () => {});
+  describe.skip("second", () => {});
 });
