c6b42f38572eec68314326c1e7de77c20f41ce7e
David de Meijere <david.de.meijere@gmail.com>
2022-12-02 08:05:59 +0100

    tweaking day 2

--- 2/index.test.ts
+++ 2/index.test.ts
@@ -1,9 +1,11 @@
 import { readFile } from "@util/readFile";
-import { Match1Outcome, Match2Outcome, run } from "./index";
+import { Match1Outcome, Match2Outcome, PlayValue, run } from "./index";
 
 describe("2", () => {
   let list: string[][];
   let testList: string[][];
+
+  let props: Parameters<typeof run>[1];
 
   beforeEach(async () => {
     list = (await readFile("./2/input.txt")).map((row) => row.split(" "));
@@ -11,73 +13,87 @@ describe("2", () => {
   });
 
   describe("first", () => {
+    beforeEach(async () => {
+      props = {
+        outcome: Match1Outcome,
+        playValue: PlayValue
+      }
+    })
+
     describe("test", () => {
       it("A Y", async () => {
         expect.assertions(1);
 
-        const result = await run([["A", "Y"]], Match1Outcome);
+        const result = await run([["A", "Y"]], props);
         expect(result).toEqual(8);
       });
       it("B X", async () => {
         expect.assertions(1);
 
-        const result = await run([["B", "X"]], Match1Outcome);
+        const result = await run([["B", "X"]], props);
         expect(result).toEqual(1);
       });
       it("C Z", async () => {
         expect.assertions(1);
 
-        const result = await run([["C", "Z"]], Match1Outcome);
+        const result = await run([["C", "Z"]], props);
         expect(result).toEqual(6);
       });
       it("test run", async () => {
         expect.assertions(1);
 
-        const result = await run(testList, Match1Outcome);
+        const result = await run(testList, props);
         expect(result).toEqual(15);
       });
     });
 
     it("run", async () => {
       expect.assertions(1);
-      const result = await run(list, Match1Outcome);
+      const result = await run(list, props);
       expect(result).toEqual(11150);
     });
   });
 
   describe("second", () => {
+    beforeEach(async () => {
+      props = {
+        outcome: Match2Outcome,
+        // playValue: PlayValue
+      }
+    })
+
     describe("test", () => {
       it("A Y", async () => {
         expect.assertions(1);
 
-        const result = await run([["A", "Y"]], Match2Outcome);
+        const result = await run([["A", "Y"]], props);
         expect(result).toEqual(4);
       });
 
       it("B X", async () => {
         expect.assertions(1);
 
-        const result = await run([["B", "X"]], Match2Outcome);
+        const result = await run([["B", "X"]], props);
         expect(result).toEqual(1);
       });
 
       it("C Z", async () => {
         expect.assertions(1);
 
-        const result = await run([["C", "Z"]], Match2Outcome);
+        const result = await run([["C", "Z"]], props);
         expect(result).toEqual(7);
       });
-      
+
       it("test run", async () => {
         expect.assertions(1);
-        const result = await run(testList, Match2Outcome);
+        const result = await run(testList, props);
         expect(result).toEqual(12);
       });
     });
 
     it("run", async () => {
       expect.assertions(1);
-      const result = await run(list, Match2Outcome);
+      const result = await run(list, props);
       expect(result).toEqual(8295);
     });
   });
--- 2/index.ts
+++ 2/index.ts
@@ -1,16 +1,26 @@
 type Outcome = { [key: string]: number };
 
-export const Match1Outcome: Outcome = {
-  AX: 4,
-  AY: 8,
-  AZ: 3,
-  BX: 1,
-  BY: 5,
-  BZ: 9,
-  CX: 7,
-  CY: 2,
-  CZ: 6,
+export const PlayValue: Outcome = {
+  A: 1, // Sten
+  B: 2, // Påse
+  C: 3, // Sax
+  X: 1, // Sten
+  Y: 2, // Påse
+  Z: 3, // Sax
 };
+
+export const Match1Outcome: Outcome = {
+  AX: 3,
+  AY: 6,
+  AZ: 0,
+  BX: 0,
+  BY: 3,
+  BZ: 6,
+  CX: 6,
+  CY: 0,
+  CZ: 3,
+};
+
 export const Match2Outcome: Outcome = {
   AX: 3,
   AY: 4,
@@ -23,10 +33,17 @@ export const Match2Outcome: Outcome = {
   CZ: 7,
 };
 
-export const run = async (rounds: string[][], outcome: Outcome) => {
+export const run = async (
+  rounds: string[][],
+  { playValue, outcome }: { playValue?: Outcome; outcome: Outcome }
+) => {
   return rounds
     .map(([a, b]) => {
-      return outcome[`${a}${b}`];
+      let play = 0;
+      if (playValue) {
+        play = playValue[b];
+      }
+      return play + outcome[`${a}${b}`];
     })
     .reduce((prev, curr) => prev + curr, 0);
 };
